1457a018e6c32c88a0709f63e344bf39d36714a7
9989840723 <sribhavya8551@gmail.com>
2024-08-31 01:12:56 +0530

    initialcommit

---
+++ src/Components/Hero/Hero.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import profile_img from '../../assets/profile_img.svg'
+import './Hero.css'
+import AnchorLink from 'react-anchor-link-smooth-scroll'
+
+const Hero = () => {
+  return (
+    <div id="home" className="hero">
+        <img src={profile_img} alt=""/>
+        <h1><span>I'm Alex Bennett,</span> frontend developer based in USA.</h1>
+        <p>I am a frontend developer from California, USA with 10 years of experience in multiple companies like Microsoft, Tesla and Apple.</p>
+        <div className="hero-action">
+            <div className="hero-connect"><AnchorLink className="anchor-link" offset={50} href="#contact">Connect with me</AnchorLink></div>
+            <div className="hero-resume">My resume</div>
+        </div>
+      
+    </div>
+
+  )
+}
+
+export default Hero
